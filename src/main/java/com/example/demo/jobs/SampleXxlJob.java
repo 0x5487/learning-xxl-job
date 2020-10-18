a8021eb1e891293e7bdc8fe42d592aa7b8fb75f5
JasonLee <jawc@hotmail.com>
2020-10-19 06:59:27 +0800

    add more sql to repo

--- src/main/java/com/example/demo/jobs/SampleXxlJob.java
+++ src/main/java/com/example/demo/jobs/SampleXxlJob.java
@@ -11,6 +11,7 @@ import com.xxl.job.core.log.XxlJobLogger;
 @Component
 public class SampleXxlJob {
 
+    // XxlJob annotation for 2.2.0
     @XxlJob("testJobHandler")
     public ReturnT<String> testJobHandler(String param) throws Exception {
         XxlJobLogger.log(" >>用戶等級更新任務開始執行....");
